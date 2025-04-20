eab1c3f75db4329a004d47382d45a166377bbc98
hymaybituin <hymaybituin@student.apc.edu.ph>
2025-04-20 19:02:18 +0800

    update userroles

--- app/Models/RoleUser.php
+++ app/Models/RoleUser.php
@@ -10,8 +10,6 @@ class RoleUser extends Model
     protected $fillable = [
         'user_id',
         'role_id',
-        'created_at',
-        'updated_at',
     ];
 
     public function user()
